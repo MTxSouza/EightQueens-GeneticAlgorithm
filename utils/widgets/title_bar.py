26b4c61be4c58741b7ba526678d3dd05b595b312
MT <msouza.os@hotmail.com>
2023-05-06 20:13:10 -0300

    create [TitleBar]: Creating title bar buttons to minimize, maximize and close the main window

--- utils/widgets/title_bar.py
+++ utils/widgets/title_bar.py
@@ -2,6 +2,11 @@
 from utils.widgets import *
 
 
+class Button(QToolButton):
+    
+    def __init__(self, *args, **kargs) -> None:
+        super(Button, self).__init__(*args, **kargs)
+
 class TitleBar(QFrame):
     
     def __init__(self, *args, **kargs) -> None:
@@ -14,6 +19,38 @@ class TitleBar(QFrame):
         # widget settings
         self.setFixedHeight(30)
         self.setStyleSheet('background: #d6d6d6')
+        
+        # widget layout
+        exitButton = Button()
+        exitButton.clicked.connect(self.exitCall)
+        
+        maxButton = Button()
+        maxButton.clicked.connect(self.maxCall)
+        
+        minButton = Button()
+        minButton.clicked.connect(self.minCall)
+        
+        mainLayout = QHBoxLayout()
+        mainLayout.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignRight)
+        mainLayout.addWidget(minButton)
+        mainLayout.addWidget(maxButton)
+        mainLayout.addWidget(exitButton)
+        self.setLayout(mainLayout)
+    
+    def exitCall(self) -> None:
+        self.root.close()
+    
+    def minCall(self) -> None:
+        if self.root.isMinimized():
+            self.root.showNormal()
+        else:
+            self.root.showMinimized()
+    
+    def maxCall(self) -> None:
+        if self.root.isFullScreen():
+            self.root.showNormal()
+        else:
+            self.root.showFullScreen()
     
     def mousePressEvent(self, event: QMouseEvent) -> None:
         if event.button() == Qt.MouseButton.LeftButton:
