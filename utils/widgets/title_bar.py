e869316bf81bf4a1a6c0ad273a3b97df9b6e9dd5
MT <msouza.os@hotmail.com>
2023-05-06 20:03:09 -0300

    create [Package]: Creating script to store the custom title bar

---
+++ utils/widgets/title_bar.py
@@ -0,0 +1,34 @@
+# imports
+from utils.widgets import *
+
+
+class TitleBar(QFrame):
+    
+    def __init__(self, *args, **kargs) -> None:
+        super(TitleBar, self).__init__(*args, **kargs)
+        
+        # main parameters
+        self.root = kargs['parent']
+        self.pressed = False
+        
+        # widget settings
+        self.setFixedHeight(30)
+        self.setStyleSheet('background: blue')
+    
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.pressed = True
+            self.mousePressedPos = event.globalPos()
+            self.windPos = self.root.pos()
+    
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        if self.pressed and not self.root.isFullScreen():
+            globalPos = event.globalPos()
+            movement = globalPos - self.mousePressedPos
+            newPos = self.windPos + movement
+            self.root.move(newPos)
+            self.update()
+    
+    def mouseReleaseEvent(self, event: QMouseEvent) -> None:
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.pressed = False
