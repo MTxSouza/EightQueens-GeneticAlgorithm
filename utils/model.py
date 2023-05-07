4914a244c3738e10157275bdabe3c7e28e255f13
MT <msouza.os@hotmail.com>
2023-05-07 00:44:42 -0300

    update [Model]: Creating method to set up the model parameters

--- utils/model.py
+++ utils/model.py
@@ -7,54 +7,20 @@ import numpy as np
 
 class EightQueensGA:
     
-    def __init__(
-        self,
-        crossover_rate: float = 0.8,
-        mutation_rate: float = 0.3, 
-        mutation_p: float = 0.3, 
-        init_population: int = 100, 
-        selection: int = None
-        ) -> None:
+    def __init__(self) -> None:
         '''
         Eight Queens Genetic Algorithm tries
         to find any way to place the 8 queens
         in a way that all of them can't capture
         each other.
-        ---
-        Args:
-            crossover_rate: Rate of crossover
-            crossover_p: Chance to happens the crossover
-            mutation_rate: Rate of mutation
-            muatation_p: Chance to happen the mutation
-            init_population: Init number of data
-            selection: Rank of the data with the best fitness
         '''
-        
-        # checking parameters
-        assert isinstance(crossover_rate, (float)), 'crossover_rate must be a float'
-        assert crossover_rate > 0 and crossover_rate <= 1, 'crossover_rate must be between 0 and 1'
-        
-        assert isinstance(mutation_rate, (float)), 'mutation_rate must be a float'
-        assert mutation_rate > 0 and mutation_rate <= 1, 'mutation_rate must be between 0 and 1'
-        
-        assert isinstance(mutation_p, (float)), 'mutation_p must be a float'
-        assert mutation_p > 0 and mutation_p <= 1, 'mutation_p must be between 0 and 1'
-        
-        assert isinstance(init_population, (int)), 'init_population must be an int'
-        assert init_population > 50, 'init_population must be bigger than 50'
-        
-        if selection is None:
-            selection = init_population // 2
-        else:
-            assert isinstance(selection, (int)), 'selection must be an int'
-            assert selection > 10 and selection < init_population, 'selection must be bigger than 10 and lower than init_population'
 
         # parameters
-        self.__crossRate = crossover_rate
-        self.__mutRate = mutation_rate
-        self.__mutP = mutation_p
-        self.__initPop = init_population
-        self.__sel = selection
+        self.__crossRate = 0.8
+        self.__mutRate = 0.3
+        self.__mutP = 0.3
+        self.__initPop = 100
+        self.__sel = 50
         
     def run(self, attempts: int = 100) -> Tuple[np.ndarray, int, bool]:
         
@@ -83,6 +49,48 @@ class EightQueensGA:
             population = self.__mutation(batch=population)
 
         return population, gen, False
+    
+    def config(
+        self,
+        crossover_rate: float = 0.8,
+        mutation_rate: float = 0.3, 
+        mutation_p: float = 0.3, 
+        init_population: int = 100, 
+        selection: int = None
+        ) -> None:
+        '''
+        Args:
+            crossover_rate: Rate of crossover
+            crossover_p: Chance to happens the crossover
+            mutation_rate: Rate of mutation
+            muatation_p: Chance to happen the mutation
+            init_population: Init number of data
+            selection: Rank of the data with the best fitness
+        '''
+        
+        # checking parameters
+        assert isinstance(crossover_rate, (float)), 'crossover_rate must be a float'
+        assert crossover_rate > 0 and crossover_rate <= 1, 'crossover_rate must be between 0 and 1'
+        
+        assert isinstance(mutation_rate, (float)), 'mutation_rate must be a float'
+        assert mutation_rate > 0 and mutation_rate <= 1, 'mutation_rate must be between 0 and 1'
+        
+        assert isinstance(mutation_p, (float)), 'mutation_p must be a float'
+        assert mutation_p > 0 and mutation_p <= 1, 'mutation_p must be between 0 and 1'
+        
+        assert isinstance(init_population, (int)), 'init_population must be an int'
+        assert init_population > 50, 'init_population must be bigger than 50'
+        
+        if selection is not None:
+            assert isinstance(selection, (int)), 'selection must be an int'
+            assert selection > 10 and selection < init_population, 'selection must be bigger than 10 and lower than init_population'
+
+        # parameters
+        self.__crossRate = crossover_rate
+        self.__mutRate = mutation_rate
+        self.__mutP = mutation_p
+        self.__initPop = init_population
+        self.__sel = selection
     
     def __genInitPopulation(self) -> np.ndarray:
         
