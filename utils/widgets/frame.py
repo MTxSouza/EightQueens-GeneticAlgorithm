4847cdce8de78942ee7341f18a93904fc653f90c
MT <msouza.os@hotmail.com>
2023-05-06 21:46:00 -0300

    update [MainFrame]: Defining design of option frame

--- utils/widgets/frame.py
+++ utils/widgets/frame.py
@@ -2,6 +2,25 @@
 from utils.widgets import *
 
 
+class OptionFrame(QFrame):
+    
+    def __init__(self, *args, **kargs) -> None:
+        super().__init__(*args, **kargs)
+        
+        # widget settings
+        self.setFixedWidth(250)
+    
+    def paintEvent(self, arg__1: QPaintEvent) -> None:
+        
+        with QPainter(self) as painter:
+            painter.setPen(Qt.PenStyle.NoPen)
+            painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+            
+            painter.setBrush(QColor('#c2c2c2'))
+            
+            rect = self.rect()
+            painter.drawRoundedRect(rect, 30, 30)
+
 class MainFrame(QFrame):
     
     def __init__(self, *args, **kargs) -> None:
@@ -11,16 +30,14 @@ class MainFrame(QFrame):
         self.setStyleSheet('background: #d6d6d6')
         
         # widget layout
-        optionsFrame = QFrame()
-        optionsFrame.setStyleSheet('background: blue')
-        optionsFrame.setFixedWidth(250)
+        optionsFrame = OptionFrame()
         
         gameFrame = QFrame()
         gameFrame.setStyleSheet('background: red')
         
         mainLayout = QHBoxLayout()
-        mainLayout.setContentsMargins(0,0,0,0)
-        mainLayout.setSpacing(0)
+        mainLayout.setContentsMargins(30,50,30,50)
+        mainLayout.setSpacing(30)
         mainLayout.addWidget(optionsFrame)
         mainLayout.addWidget(gameFrame)
         self.setLayout(mainLayout)
