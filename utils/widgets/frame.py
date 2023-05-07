7669f9c39acb75e1ec0c216d973e8fa9bc34fbc8
MT <msouza.os@hotmail.com>
2023-05-06 23:49:04 -0300

    create [OptionFrame]: Creating custom sliders to set up the model parameters before run the genetic algorithm

--- utils/widgets/frame.py
+++ utils/widgets/frame.py
@@ -2,13 +2,125 @@
 from utils.widgets import *
 
 
+class Slider(QSlider):
+    
+    def __init__(self, min: int, max: int, isFloat: bool, *args, **kargs):
+        super(Slider, self).__init__(*args, **kargs)
+        
+        # widget settings
+        self.setOrientation(Qt.Orientation.Horizontal)
+        self.setRange(min, max)
+        
+        # main parameters
+        self.f = isFloat
+    
+    def paintEvent(self, event):
+        
+        with QPainter(self) as painter:
+            painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+            painter.setPen(Qt.PenStyle.NoPen)
+
+            sliderRect = QRect(0, self.height() // 2 - 2, self.width(), 2)
+            
+            painter.setBrush(QColor("#d6d6d6"))
+            painter.drawRect(sliderRect)
+
+            trackWidth = (self.value() - self.minimum()) / (self.maximum() - self.minimum()) * (self.width() - 16)
+            trackRect = QRect(0, self.height() // 2 - 2, trackWidth, 2)
+            
+            painter.setBrush(QColor("#00a3cc"))
+            painter.drawRect(trackRect)
+
+            cursorWidth = 16
+            cursorHeight = 16
+            cursorRect = QRect(trackWidth, self.height() // 2 - cursorHeight // 2, cursorWidth, cursorHeight)
+            
+            painter.setBrush(QColor("#00a3cc"))
+            painter.drawEllipse(cursorRect)
+    
+    def getValue(self) -> int:
+        if self.f:
+            return self.value() / 100
+        return self.value()
+    
+class OptionField(QFrame):
+    
+    def __init__(self, parameter: str, *args, **kargs) -> None:
+        super(OptionField, self).__init__(*args, **kargs)
+        
+        # widget settings
+        self.setFixedHeight(50)
+        self.setStyleSheet('background: #c2c2c2')
+        
+        # widget layout
+        if parameter in ['Mutation Rate', 'Crossover Rate', 'Mutation Prob.', 'Crossover Prob.']:
+            self.slider = Slider(min=0, max=100, isFloat=True)
+        else:
+            self.slider = Slider(min=50, max=200, isFloat=False)
+        
+        font = QFont()
+        font.setBold(True)
+        font.setFamily('Arial')
+        font.setPointSize(10)
+        
+        title = QLabel()
+        title.setFont(font)
+        title.setText(parameter)
+        
+        self.sliderText = QLabel()
+        self.sliderText.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.sliderText.setFont(font)
+        self.slider.valueChanged.connect(self.updateText)
+        self.sliderText.setText(str(self.slider.value()))
+        
+        textLayout = QHBoxLayout()
+        textLayout.setContentsMargins(0,0,0,0)
+        textLayout.setSpacing(0)
+        textLayout.addWidget(title)
+        textLayout.addWidget(self.sliderText)
+        
+        textFrame = QFrame()
+        textFrame.setLayout(textLayout)
+        
+        mainLayout = QVBoxLayout()
+        mainLayout.setContentsMargins(2,2,2,2)
+        mainLayout.setSpacing(0)
+        mainLayout.addWidget(textFrame)
+        mainLayout.addWidget(self.slider)
+        self.setLayout(mainLayout)
+    
+    def updateText(self) -> None:
+        self.sliderText.setText(str(self.slider.getValue()))
+
 class OptionFrame(QFrame):
     
     def __init__(self, *args, **kargs) -> None:
-        super().__init__(*args, **kargs)
+        super(OptionFrame, self).__init__(*args, **kargs)
         
         # widget settings
         self.setFixedWidth(250)
+        
+        # widget layout
+        mutationRate = OptionField(parameter='Mutation Rate')
+        
+        mutationProb = OptionField(parameter='Mutation Prob.')
+        
+        crossoverRate = OptionField(parameter='Crossover Rate')
+        
+        crossoverProb = OptionField(parameter='Crossover Prob.')
+        
+        population = OptionField(parameter='Init Population')
+        
+        mainLayout = QVBoxLayout()
+        mainLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        mainLayout.setContentsMargins(20,50,20,50)
+        mainLayout.setSpacing(20)
+        mainLayout.addWidget(mutationRate)
+        mainLayout.addWidget(mutationProb)
+        mainLayout.addWidget(crossoverRate)
+        mainLayout.addWidget(crossoverProb)
+        mainLayout.addWidget(population)
+        self.setLayout(mainLayout)
     
     def paintEvent(self, arg__1: QPaintEvent) -> None:
         
